f906610a90ef9f2cfe8bcfa85ad40237230c241f
sam <sam.brougham@gmail.com>
2025-01-11 11:03:41 -0500

    Improve error handling

--- smoke_test/src/main.rs
+++ smoke_test/src/main.rs
@@ -1,26 +1,32 @@
+use shared::pool::ThreadPool;
 use std::{
-    io::{prelude::*, BufReader},
+    io::{prelude::*, BufReader, Result},
     net::{TcpListener, TcpStream},
 };
-use shared::pool::ThreadPool;
 
-fn main() {
-    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
+fn main() -> Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8080")?;
     let pool = ThreadPool::new(5);
 
     for stream in listener.incoming() {
-        let stream = stream.unwrap();
+        let stream = stream?;
 
         pool.execute(|| {
-            handle_connection(stream);
+            if let Err(e) = handle_connection(stream) {
+                eprintln!("Connection error: {}", e);
+            };
         });
     }
+
+    Ok(())
 }
 
-fn handle_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) -> Result<()> {
     let mut buf_reader = BufReader::new(&stream);
     let mut buffer = Vec::new();
 
-    buf_reader.read_to_end(&mut buffer).expect("Error reading stream");
-    stream.write_all(&buffer).unwrap();
+    buf_reader.read_to_end(&mut buffer)?;
+    stream.write_all(&buffer)?;
+
+    Ok(())
 }
